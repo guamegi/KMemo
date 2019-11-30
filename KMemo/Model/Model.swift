ced42199fb9a1ee94409138271961701cf8a9203
Minha Kwon <namisoft@naver.com>
2019-11-30 23:42:39 +0900

    Memo Class

---
+++ KMemo/Model/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  KMemo
+//
+//  Created by 나미콘 on 2019/11/30.
+//  Copyright © 2019 나미콘. All rights reserved.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "Lorem Ipsum"),
+        Memo(content: "Subscribe + ?")
+    ]
+}
