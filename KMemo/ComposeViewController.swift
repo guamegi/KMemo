512fb78b346749ae40cae17f3a1f84eaeb1d4342
Minha Kwon <namisoft@naver.com>
2019-12-01 01:02:06 +0900

    새 메모 화면

---
+++ KMemo/ComposeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ComposeViewController.swift
+//  KMemo
+//
+//  Created by 나미콘 on 2019/12/01.
+//  Copyright © 2019 나미콘. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
